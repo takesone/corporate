ca367672c98fee3d78e4b6341afd77cc83835e80
sengoku kizuku <a15dc095@dhw.ac.jp>
2016-08-09 16:17:50 +0900

    First commit

---
+++ js/top.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+(function($) {
+    $(function() {
+        var $header = $('#top-head');
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 600) {
+                $header.addClass('fixed');
+            } else {
+                $header.removeClass('fixed');
+            }
+        });
+    });
+
+    $(window).load(function() {
+        function changeWidget() {
+            var twFrame = $('iframe.twitter-timeline');
+            if (twFrame.length > 0) {
+                twFrame.contents()
+                    .find('h1').text('すなすなのツイート')
+                    .css('font-size','16px');
+            } else {
+                setTimeout(changeWidget, 500);
+            }
+        }
+        changeWidget();
+    });
+
+
+
+})(jQuery);
