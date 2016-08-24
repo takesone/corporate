e7c52c606f2c1ca8a9a2d288661be3ebbb4a0a7b
Yusuke Sunada <a15dc093@dhw.ac.jp>
2016-08-24 21:32:44 +0900

    Q&Aのcssをsassに変更

---
+++ dist/js/qa.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+
+$(function () {
+	var browserWidth = $(window).width();
+	
+	$(".question").on("click", function() {
+			
+		var drop = $(this).find('.question-text');
+		
+		drop.slideToggle()
+		.toggleClass("active");
+			
+	});
+	
+
+	$(".drop_active").on("click", function() {
+		
+		if(browserWidth >= 960){
+		
+			return false;
+		
+		}
+		
+		var drop = $('.response_drop');
+		
+		drop.slideToggle()
+		.toggleClass("active");
+		
+	});
+	
+	$(window).on('load resize', function(){
+		browserWidth = $(window).width();
+		
+		if (browserWidth >= 960){
+			
+			$('.response_drop').slideDown();
+			
+		}else{
+		
+			$('.response_drop').slideUp();
+			
+		}
+		
+	});
+	
+});
