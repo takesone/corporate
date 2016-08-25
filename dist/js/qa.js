612369d238a496f3cbd29db3267825c1da71aec1
Yusuke Sunada <a15dc093@dhw.ac.jp>
2016-08-25 19:23:10 +0900

    qaのプラス、マイナスボタンの作成

--- dist/js/qa.js
+++ dist/js/qa.js
@@ -2,45 +2,61 @@
 
 $(function () {
 	var browserWidth = $(window).width();
-	
+
 	$(".question").on("click", function() {
-			
-		var drop = $(this).find('.question-text');
-		
+
+		var drop = $(this).find('.question_text');
+
 		drop.slideToggle()
 		.toggleClass("active");
-			
+
 	});
-	
+
 
 	$(".drop_active").on("click", function() {
-		
+
 		if(browserWidth >= 960){
-		
-			return false;
-		
+
 		}
-		
+
 		var drop = $('.response_drop');
-		
+
 		drop.slideToggle()
 		.toggleClass("active");
-		
+
 	});
-	
+
 	$(window).on('load resize', function(){
 		browserWidth = $(window).width();
-		
+
 		if (browserWidth >= 960){
-			
+
 			$('.response_drop').slideDown();
-			
+
 		}else{
-		
+
 			$('.response_drop').slideUp();
-			
+
 		}
-		
+
 	});
-	
+
+
+
+	$(".question").on("click", function() {
+		// activeが存在する場合
+
+var plus = $(this).find('.accordion_icon');
+
+		if ($(plus).hasClass('active')) {
+			// activeを削除
+			$(plus).removeClass('active');
+		}
+		else {
+			// activeを追加
+			$(plus).addClass('active');
+		}
+
+	});
+
 });
